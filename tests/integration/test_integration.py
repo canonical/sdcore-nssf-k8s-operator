b611b9e835bde04dbfaab248f1101d43eb20342e
Bartlomiej Gmerek <42570669+Gmerold@users.noreply.github.com>
2023-09-01 10:50:13 +0200

    (TELCO-574) Block charm when NRF goes down (#17)

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -9,6 +9,7 @@ from pathlib import Path
 
 import pytest
 import yaml
+from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
@@ -19,8 +20,8 @@ DB_APPLICATION_NAME = "mongodb-k8s"
 NRF_APPLICATION_NAME = "sdcore-nrf"
 
 
-async def _deploy_mongodb(ops_test):
-    await ops_test.model.deploy(
+async def _deploy_mongodb(ops_test: OpsTest):
+    await ops_test.model.deploy(  # type: ignore[union-attr]
         DB_APPLICATION_NAME,
         application_name=DB_APPLICATION_NAME,
         channel="5/edge",
@@ -28,22 +29,22 @@ async def _deploy_mongodb(ops_test):
     )
 
 
-async def _deploy_sdcore_nrf_operator(ops_test):
+async def _deploy_sdcore_nrf_operator(ops_test: OpsTest):
     await _deploy_mongodb(ops_test)
-    await ops_test.model.deploy(
+    await ops_test.model.deploy(  # type: ignore[union-attr]
         NRF_APPLICATION_NAME,
         application_name=NRF_APPLICATION_NAME,
         channel="edge",
         trust=True,
     )
-    await ops_test.model.add_relation(
+    await ops_test.model.add_relation(  # type: ignore[union-attr]
         relation1=DB_APPLICATION_NAME, relation2=NRF_APPLICATION_NAME
     )
 
 
 @pytest.fixture(scope="module")
 @pytest.mark.abort_on_fail
-async def build_and_deploy(ops_test):
+async def build_and_deploy(ops_test: OpsTest):
     """Build the charm-under-test and deploy it."""
     deploy_nrf = asyncio.create_task(_deploy_sdcore_nrf_operator(ops_test))
     charm = await ops_test.build_charm(".")
@@ -51,7 +52,7 @@ async def build_and_deploy(ops_test):
     resources = {
         "nssf-image": METADATA["resources"]["nssf-image"]["upstream-source"],
     }
-    await ops_test.model.deploy(
+    await ops_test.model.deploy(  # type: ignore[union-attr]
         charm,
         resources=resources,
         application_name=APP_NAME,
@@ -60,13 +61,31 @@ async def build_and_deploy(ops_test):
 
 
 @pytest.mark.abort_on_fail
-async def test_relate_and_wait_for_active_status(
-    ops_test,
-    build_and_deploy,
-):
-    await ops_test.model.add_relation(relation1=APP_NAME, relation2=NRF_APPLICATION_NAME)
-    await ops_test.model.wait_for_idle(
+async def test_relate_and_wait_for_active_status(ops_test: OpsTest, build_and_deploy):
+    await ops_test.model.add_relation(relation1=APP_NAME, relation2=NRF_APPLICATION_NAME)  # type: ignore[union-attr]  # noqa: E501
+    await ops_test.model.wait_for_idle(  # type: ignore[union-attr]
         apps=[APP_NAME],
         status="active",
         timeout=1000,
     )
+
+
+@pytest.mark.abort_on_fail
+async def test_remove_nrf_and_wait_for_blocked_status(ops_test: OpsTest, build_and_deploy):
+    await ops_test.model.remove_application(NRF_APPLICATION_NAME, block_until_done=True)  # type: ignore[union-attr]  # noqa: E501
+    await ops_test.model.wait_for_idle(apps=[APP_NAME], status="blocked", timeout=60)  # type: ignore[union-attr]  # noqa: E501
+
+
+@pytest.mark.abort_on_fail
+async def test_restore_nrf_and_wait_for_active_status(ops_test: OpsTest, build_and_deploy):
+    await ops_test.model.deploy(  # type: ignore[union-attr]
+        NRF_APPLICATION_NAME,
+        application_name=NRF_APPLICATION_NAME,
+        channel="edge",
+        trust=True,
+    )
+    await ops_test.model.add_relation(  # type: ignore[union-attr]
+        relation1=f"{NRF_APPLICATION_NAME}:database", relation2=f"{DB_APPLICATION_NAME}"
+    )
+    await ops_test.model.add_relation(relation1=APP_NAME, relation2=NRF_APPLICATION_NAME)  # type: ignore[union-attr]  # noqa: E501
+    await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=300)  # type: ignore[union-attr]  # noqa: E501
