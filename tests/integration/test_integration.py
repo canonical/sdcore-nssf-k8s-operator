dab1d9c620aaa16533d56a61acc0e931177f6d59
Bartlomiej Gmerek <42570669+Gmerold@users.noreply.github.com>
2023-09-21 09:12:53 +0200

    Enable auto-merging Renovate PRs (#21)

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -88,4 +88,4 @@ async def test_restore_nrf_and_wait_for_active_status(ops_test: OpsTest, build_a
         relation1=f"{NRF_APPLICATION_NAME}:database", relation2=f"{DB_APPLICATION_NAME}"
     )
     await ops_test.model.add_relation(relation1=APP_NAME, relation2=NRF_APPLICATION_NAME)  # type: ignore[union-attr]  # noqa: E501
-    await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=300)  # type: ignore[union-attr]  # noqa: E501
+    await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=1000)  # type: ignore[union-attr]  # noqa: E501
