0beabf7e09faf53baaed40f3bc5b47246a23892d
Bartlomiej Gmerek <42570669+Gmerold@users.noreply.github.com>
2024-12-18 09:54:08 +0100

    fix: Uses ubuntu@24.04 in TF module and itests (#384)

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,7 @@ resource "juju_application" "nssf" {
     name     = "sdcore-nssf-k8s"
     channel  = var.channel
     revision = var.revision
+    base     = var.base
   }
 
   config      = var.config
